77665f54ce2d8eeaf5c2b0e54d3db5d45a8962e4
ssarwat <sumaiyah_sarwat@yahoo.com>
2019-08-21 11:01:50 -0400

    first commit

---
+++ src/main/java/com/example/demo/Ex303Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex303Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex303Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,61 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min = 2)
+    private String task;
+
+    @NotNull
+    @Size(min = 3)
+    private String dueDate;
+
+    @NotNull
+    @Size(min = 3)
+    private String priority;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+}
